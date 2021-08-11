b8218bdf5f47d3742abe18a5bf2a29b6a8e54487
Gage Eide <gageeide@gmail.com>
2021-08-11 00:07:02 -0500

    Added siteRecipe Seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -23,7 +23,182 @@ mongoose.connect('mongodb://localhost/meal-planner', {
 
 const siteRecipeSeeds = [
   {
-
+    _id: "5b9b8f0f5a2c3d1f9c9e0c5b",
+    name: 'ChickenSimply',
+    instructions: '',
+    ingredients: [{
+        name: 'Chicken',
+        quantity: 1,
+        unit: 'lb'
+      }, {
+        name: 'Salt',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Pepper',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Onion',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Red Onions',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Celery',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Black Pepper',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Kale',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Cilantro',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Lime',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Lemon',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Sage',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Coriander',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Horseradish',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Turmeric',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Garlic Cleric',
+        quantity: 1,
+        unit: 'tsp'
+      }
+    ],
+    tags: ['chicken', 'soup', 'healthy', 'low-calorie', 'low-carb', 'low-fat', 'low-sodium', 'low-sugar'],
+    user: { _id: '5b4c8b8c8e9b7e6c7b7c8d6c'},
+  },
+  {
+    _id: "5b9b8f0f5a2c3d1f9c9e0c5c",
+    name: 'Beef With Soy',
+    instructions: '',
+    ingredients: [{
+        name: 'Beef',
+        quantity: 1,
+        unit: 'lb'
+      }, {
+        name: 'Salt',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Pepper',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Onion',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Red Onions',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Celery',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Black Pepper',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Cilantro',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Lime',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Lemon',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Sage',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Turmeric',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Garlic Cleric',
+        quantity: 1,
+        unit: 'tsp'
+      }
+    ],
+    tags: ['beef', 'healthy', 'low-calorie', 'low-carb', 'low-fat', 'high-protein', 'low-sugar'],
+    user: { _id: '5a1b8b8c8e9b7e6c7b7c8f0c'},
+  },
+  {
+    name: "Sausage Bolognese",
+    instructions: '',
+    ingredients: [{
+        name: 'Italian Sausage',
+        quantity: 1,
+        unit: 'lb'
+      }, {
+        name: 'Salt',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Pepper',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Onion',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Red Onions',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Black Pepper',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Cilantro',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Sage',
+        quantity: 1,
+        unit: 'tsp'
+      }, {
+        name: 'Garlic Cloves',
+        quantity: 1,
+        unit: ''
+      }
+    ],
+    tags: ['italian', 'sausage', 'healthy', 'low-fat', 'low-sugar'],
+    user: { _id: 'f7vb8b8c8e9b7e6c7b7c88cy'}
   }
 ];
 
@@ -57,6 +232,7 @@ const siteRecipeSeeds = [
 
 const userSeeds = [
   {
+    _id: "5a1b8b8c8e9b7e6c7b7c8f0c",
     username: 'user1',
     email: "email@email.com",
     password: "password",
@@ -106,6 +282,7 @@ const userSeeds = [
     },
   },
   {
+    _id: "f7vb8b8c8e9b7e6c7b7c88cy",
     username: 'user2',
     email: "moreemail@mail.com",
         password: "password",
