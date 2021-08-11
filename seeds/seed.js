940f1f7691a7338f509c6904f718003a8f461a52
Gage Eide <gageeide@gmail.com>
2021-08-10 23:54:07 -0500

    Added user seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,37 +7,158 @@ mongoose.connect('mongodb://localhost/meal-planner', {
   useUnifiedTopology: true,
 });
 
-const recipe = {
-  name: 'Recipe',
-  ingredients: [
-    {
-      name: 'Ingredient 1',
-      amount: '1',
-      unit: 'unit',
-    },
-    {
-      name: 'Ingredient 2',
-      amount: '2',
-      unit: 'unit',
-    },
-  ],
-  instructions: [
-    {
-      step: 'Step 1',
-      description: 'Description',
-    },
-    {
-      step: 'Step 2',
-      description: 'Description',
-    },
-  ],
-};
+/* siteRecipeSeed template: 
+  {
+    name: "",
+    instructions: "",
+    ingredients: [{
+      name: String,
+      quantity: Number,
+      unit: String
+    }],
+    tags: [String],
+    user: _id: Schema.Types.ObjectId,
+  }
+*/
 
+const siteRecipeSeeds = [
+  {
 
+  }
+];
+
+/* userSeed template:
+  {
+    username: "",
+    email: "",
+    password: "",
+    recipes: {
+      personalRecipes: [
+        {
+          name: "",
+          instructions: "",
+          ingredients: [
+            {
+              name: String,
+              quantity: Number,
+              unit: String,
+            },
+          ],
+        },
+      ],
+      siteRecipes: [
+        {
+            _id: Schema.Types.ObjectId,
+        }
+      ],
+    },
+  }
+*/
+
+const userSeeds = [
+  {
+    username: 'user1',
+    email: "email@email.com",
+    password: "password",
+    recipes: {
+      personalRecipes: [
+        {
+          name: "Recipe 1",
+          instructions: "Instructions 1",
+          ingredients: [
+            {
+              name: "Ingredient 1",
+              quantity: 1,
+              unit: "unit 1",
+            },
+            {
+              name: "Ingredient 2",
+              quantity: 2,
+              unit: "unit 2",
+            },
+          ],
+        },
+        {
+          name: "Recipe 2",
+          instructions: "Instructions 2",
+          ingredients: [
+            {
+              name: "Ingredient 1",
+              quantity: 1,
+              unit: "unit 1",
+            },
+            {
+              name: "Ingredient 2",
+              quantity: 2,
+              unit: "unit 2",
+            },
+          ],
+        },
+      ],
+      siteRecipes: [
+        {
+          _id: "5b9b8f0f5a2c3d1f9c9e0c5b",
+        },
+        {
+          _id: "5b9b8f0f5a2c3d1f9c9e0c5c",
+        },
+      ],
+    },
+  },
+  {
+    username: 'user2',
+    email: "moreemail@mail.com",
+        password: "password",
+    recipes: {
+      personalRecipes: [
+        {
+          name: "Recipe 1",
+          instructions: "Instructions 1",
+          ingredients: [
+            {
+              name: "Ingredient 1",
+              quantity: 1,
+              unit: "unit 1",
+            },
+            {
+              name: "Ingredient 2",
+              quantity: 2,
+              unit: "unit 2",
+            },
+          ],
+        },
+        {
+          name: "Recipe 2",
+          instructions: "Instructions 2",
+          ingredients: [
+            {
+              name: "Ingredient 1",
+              quantity: 1,
+              unit: "unit 1",
+            },
+            {
+              name: "Ingredient 2",
+              quantity: 2,
+              unit: "unit 2",
+            },
+          ],
+        },
+      ],
+      siteRecipes: [
+        {
+          _id: "5b9b8f0f5a2c3d1f9c9e0c5b",
+        },
+        {
+          _id: "5b9b8f0f5a2c3d1f9c9e0c5c",
+        },
+      ],
+    },
+  },
+];
 
 // !IMPORTANT: Adjust Schema before running this seed
-db.Mmo.deleteMany({})
-  .then(() => db.Mmo.collection.insertMany(mmoSeed))
+db.User.deleteMany({})
+  .then(() => db.User.collection.insertMany(mmoSeed))
   .then((data) => {
     console.log(data.result.n + ' records inserted!');
     process.exit(0);
