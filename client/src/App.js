c99df5d846b2da1c1cd2f9d46360c1ce9b0e7291
Gage Eide <gageeide@gmail.com>
2021-08-10 22:42:45 -0500

    Copied over my boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
