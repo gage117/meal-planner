cdc13fb3eeb4a5a8ccbee4bb8ad1e0a96fe34240
Gage Eide <gageeide@gmail.com>
2021-08-11 01:02:08 -0500

    Finished working with seeds for the night

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const db = require('../models');
+const mongoose = require("mongoose");
+const db = require("../models");
 
-mongoose.connect('mongodb://localhost/meal-planner', {
+mongoose.connect("mongodb://localhost/meal-planner", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
@@ -37,7 +37,7 @@ mongoose.connect('mongodb://localhost/meal-planner', {
 
 const userSeeds = [
   {
-    username: 'user1',
+    username: "user1",
     email: "email@email.com",
     password: "password",
     personalRecipes: [
@@ -77,7 +77,7 @@ const userSeeds = [
     siteRecipes: [],
   },
   {
-    username: 'user2',
+    username: "user2",
     email: "moreemail@mail.com",
     password: "password",
     personalRecipes: [
@@ -134,210 +134,282 @@ const userSeeds = [
 
 const siteRecipeSeeds = [
   {
-    name: 'ChickenSimply',
-    instructions: '',
-    ingredients: [{
-        name: 'Chicken',
+    name: "ChickenSimply",
+    instructions: "",
+    ingredients: [
+      {
+        name: "Chicken",
         quantity: 1,
-        unit: 'lb'
-      }, {
-        name: 'Salt',
+        unit: "lb",
+      },
+      {
+        name: "Salt",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Pepper',
+        unit: "tsp",
+      },
+      {
+        name: "Pepper",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Onion',
+        unit: "tsp",
+      },
+      {
+        name: "Onion",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Red Onions',
+        unit: "tsp",
+      },
+      {
+        name: "Red Onions",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Celery',
+        unit: "tsp",
+      },
+      {
+        name: "Celery",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Black Pepper',
+        unit: "tsp",
+      },
+      {
+        name: "Black Pepper",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Kale',
+        unit: "tsp",
+      },
+      {
+        name: "Kale",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Cilantro',
+        unit: "tsp",
+      },
+      {
+        name: "Cilantro",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Lime',
+        unit: "tsp",
+      },
+      {
+        name: "Lime",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Lemon',
+        unit: "tsp",
+      },
+      {
+        name: "Lemon",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Sage',
+        unit: "tsp",
+      },
+      {
+        name: "Sage",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Coriander',
+        unit: "tsp",
+      },
+      {
+        name: "Coriander",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Horseradish',
+        unit: "tsp",
+      },
+      {
+        name: "Horseradish",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Turmeric',
+        unit: "tsp",
+      },
+      {
+        name: "Turmeric",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Garlic Cleric',
+        unit: "tsp",
+      },
+      {
+        name: "Garlic Cleric",
         quantity: 1,
-        unit: 'tsp'
-      }
+        unit: "tsp",
+      },
     ],
-    tags: ['chicken', 'soup', 'healthy', 'low-calorie', 'low-carb', 'low-fat', 'low-sodium', 'low-sugar'],
-    user: { _id: ''},
+    tags: [
+      "chicken",
+      "soup",
+      "healthy",
+      "low-calorie",
+      "low-carb",
+      "low-fat",
+      "low-sodium",
+      "low-sugar",
+    ],
+    user: { _id: "" },
   },
   {
-    name: 'Beef With Soy',
-    instructions: '',
-    ingredients: [{
-        name: 'Beef',
+    name: "Beef With Soy",
+    instructions: "",
+    ingredients: [
+      {
+        name: "Beef",
         quantity: 1,
-        unit: 'lb'
-      }, {
-        name: 'Salt',
+        unit: "lb",
+      },
+      {
+        name: "Salt",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Pepper',
+        unit: "tsp",
+      },
+      {
+        name: "Pepper",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Onion',
+        unit: "tsp",
+      },
+      {
+        name: "Onion",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Red Onions',
+        unit: "tsp",
+      },
+      {
+        name: "Red Onions",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Celery',
+        unit: "tsp",
+      },
+      {
+        name: "Celery",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Black Pepper',
+        unit: "tsp",
+      },
+      {
+        name: "Black Pepper",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Cilantro',
+        unit: "tsp",
+      },
+      {
+        name: "Cilantro",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Lime',
+        unit: "tsp",
+      },
+      {
+        name: "Lime",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Lemon',
+        unit: "tsp",
+      },
+      {
+        name: "Lemon",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Sage',
+        unit: "tsp",
+      },
+      {
+        name: "Sage",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Turmeric',
+        unit: "tsp",
+      },
+      {
+        name: "Turmeric",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Garlic Cleric',
+        unit: "tsp",
+      },
+      {
+        name: "Garlic Cleric",
         quantity: 1,
-        unit: 'tsp'
-      }
+        unit: "tsp",
+      },
     ],
-    tags: ['beef', 'healthy', 'low-calorie', 'low-carb', 'low-fat', 'high-protein', 'low-sugar'],
-    user: { _id: ''},
+    tags: [
+      "beef",
+      "healthy",
+      "low-calorie",
+      "low-carb",
+      "low-fat",
+      "high-protein",
+      "low-sugar",
+    ],
+    user: { _id: "" },
   },
   {
     name: "Sausage Bolognese",
-    instructions: '',
-    ingredients: [{
-        name: 'Italian Sausage',
+    instructions: "",
+    ingredients: [
+      {
+        name: "Italian Sausage",
         quantity: 1,
-        unit: 'lb'
-      }, {
-        name: 'Salt',
+        unit: "lb",
+      },
+      {
+        name: "Salt",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Pepper',
+        unit: "tsp",
+      },
+      {
+        name: "Pepper",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Onion',
+        unit: "tsp",
+      },
+      {
+        name: "Onion",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Red Onions',
+        unit: "tsp",
+      },
+      {
+        name: "Red Onions",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Black Pepper',
+        unit: "tsp",
+      },
+      {
+        name: "Black Pepper",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Cilantro',
+        unit: "tsp",
+      },
+      {
+        name: "Cilantro",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Sage',
+        unit: "tsp",
+      },
+      {
+        name: "Sage",
         quantity: 1,
-        unit: 'tsp'
-      }, {
-        name: 'Garlic Cloves',
+        unit: "tsp",
+      },
+      {
+        name: "Garlic Cloves",
         quantity: 1,
-        unit: ''
-      }
+        unit: "",
+      },
     ],
-    tags: ['italian', 'sausage', 'healthy', 'low-fat', 'low-sugar'],
-    user: { _id: ''}
-  }
+    tags: ["italian", "sausage", "healthy", "low-fat", "low-sugar"],
+    user: { _id: "" },
+  },
 ];
 
 // !IMPORTANT: Adjust Schema before running this seed
 const seedDB = async () => {
   try {
     // Seed Users
-    await db.User.deleteMany({})
-    const userData = await db.User.collection.insertMany(userSeeds)
-    console.log('Seeded:', userData.ops.length, 'users')
-    // Seed SiteRecipes
-    await db.SiteRecipe.deleteMany({})
-    // Use seeded user IDs to seed recipes with users
-    siteRecipeSeeds[0].user = userData.insertedIds["0"]
-    siteRecipeSeeds[1].user = userData.insertedIds["1"]
-    siteRecipeSeeds[2].user = userData.insertedIds["1"]
-    const recipeData = await db.SiteRecipe.collection.insertMany(siteRecipeSeeds)
-    console.log(recipeData)
-    console.log('Seeded:', recipeData.ops.length, 'recipes')
-    const something = await db.User.findOneAndUpdate(
-      { _id: userData.insertedIds["0"] },
-      { $push: {
-        
+    await db.User.deleteMany({});
+    const userData = await db.User.collection.insertMany(userSeeds);
+    console.log("Seeded:", userData.ops.length, "users");
 
+    // Seed SiteRecipes
+    await db.SiteRecipe.deleteMany({});
+    // Use seeded user IDs to seed recipes with users
+    siteRecipeSeeds[0].user = userData.insertedIds["0"];
+    siteRecipeSeeds[1].user = userData.insertedIds["1"];
+    siteRecipeSeeds[2].user = userData.insertedIds["1"];
+    const recipeData = await db.SiteRecipe.collection.insertMany(
+      siteRecipeSeeds
+    );
+    console.log("Seeded:", recipeData.ops.length, "recipes");
+
+    // Append each user's siteRecipes with newly created siteRecipe IDs
+    const updatedUser1 = await db.User.findOneAndUpdate(
+      { _id: userData.insertedIds["0"] },
+      {
+        $push: {
+          siteRecipes: { $each: [recipeData.insertedIds["0"], recipeData.insertedIds["1"]] },
+        },
+      }
+    );
+    const updatedUser2 = await db.User.findOneAndUpdate(
+      { _id: userData.insertedIds["0"] },
+      {
+        $push: {
+          siteRecipes: { $each: [recipeData.insertedIds["0"], recipeData.insertedIds["2"]] },
+        },
+      }
+    );
+    console.log(updatedUser1);
+    console.log(updatedUser2);
+    
     process.exit(0);
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err);
     process.exit(1);
   }
-}
+};
 
-seedDB()
+seedDB();
