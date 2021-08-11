f9c2bb517b849c2550ef5ad74c3e1c3d6d05aea2
Gage Eide <gageeide@gmail.com>
2021-08-11 00:55:26 -0500

    Adjusted seeds to match changes to User schema

--- seeds/seed.js
+++ seeds/seed.js
@@ -40,99 +40,81 @@ const userSeeds = [
     username: 'user1',
     email: "email@email.com",
     password: "password",
-    recipes: {
-      personalRecipes: [
-        {
-          name: "Recipe 1",
-          instructions: "Instructions 1",
-          ingredients: [
-            {
-              name: "Ingredient 1",
-              quantity: 1,
-              unit: "unit 1",
-            },
-            {
-              name: "Ingredient 2",
-              quantity: 2,
-              unit: "unit 2",
-            },
-          ],
-        },
-        {
-          name: "Recipe 2",
-          instructions: "Instructions 2",
-          ingredients: [
-            {
-              name: "Ingredient 1",
-              quantity: 1,
-              unit: "unit 1",
-            },
-            {
-              name: "Ingredient 2",
-              quantity: 2,
-              unit: "unit 2",
-            },
-          ],
-        },
-      ],
-      siteRecipes: [
-        {
-          _id: "5b9b8f0f5a2c3d1f9c9e0c5b",
-        },
-        {
-          _id: "5b9b8f0f5a2c3d1f9c9e0c5c",
-        },
-      ],
-    },
+    personalRecipes: [
+      {
+        name: "Recipe 1",
+        instructions: "Instructions 1",
+        ingredients: [
+          {
+            name: "Ingredient 1",
+            quantity: 1,
+            unit: "unit 1",
+          },
+          {
+            name: "Ingredient 2",
+            quantity: 2,
+            unit: "unit 2",
+          },
+        ],
+      },
+      {
+        name: "Recipe 2",
+        instructions: "Instructions 2",
+        ingredients: [
+          {
+            name: "Ingredient 1",
+            quantity: 1,
+            unit: "unit 1",
+          },
+          {
+            name: "Ingredient 2",
+            quantity: 2,
+            unit: "unit 2",
+          },
+        ],
+      },
+    ],
+    siteRecipes: [],
   },
   {
     username: 'user2',
     email: "moreemail@mail.com",
     password: "password",
-    recipes: {
-      personalRecipes: [
-        {
-          name: "Recipe 1",
-          instructions: "Instructions 1",
-          ingredients: [
-            {
-              name: "Ingredient 1",
-              quantity: 1,
-              unit: "unit 1",
-            },
-            {
-              name: "Ingredient 2",
-              quantity: 2,
-              unit: "unit 2",
-            },
-          ],
-        },
-        {
-          name: "Recipe 2",
-          instructions: "Instructions 2",
-          ingredients: [
-            {
-              name: "Ingredient 1",
-              quantity: 1,
-              unit: "unit 1",
-            },
-            {
-              name: "Ingredient 2",
-              quantity: 2,
-              unit: "unit 2",
-            },
-          ],
-        },
-      ],
-      siteRecipes: [
-        {
-          _id: "5b9b8f0f5a2c3d1f9c9e0c5b",
-        },
-        {
-          _id: "5b9b8f0f5a2c3d1f9c9e0c5c",
-        },
-      ],
-    },
+    personalRecipes: [
+      {
+        name: "Recipe 1",
+        instructions: "Instructions 1",
+        ingredients: [
+          {
+            name: "Ingredient 1",
+            quantity: 1,
+            unit: "unit 1",
+          },
+          {
+            name: "Ingredient 2",
+            quantity: 2,
+            unit: "unit 2",
+          },
+        ],
+      },
+      {
+        name: "Recipe 2",
+        instructions: "Instructions 2",
+        ingredients: [
+          {
+            name: "Ingredient 1",
+            quantity: 1,
+            unit: "unit 1",
+          },
+          {
+            name: "Ingredient 2",
+            quantity: 2,
+            unit: "unit 2",
+          },
+        ],
+      },
+    ],
+    siteRecipes: [],
   },
 ];
 
@@ -335,16 +317,21 @@ const seedDB = async () => {
     // Seed Users
     await db.User.deleteMany({})
     const userData = await db.User.collection.insertMany(userSeeds)
-    console.log(userData)
     console.log('Seeded:', userData.ops.length, 'users')
     // Seed SiteRecipes
     await db.SiteRecipe.deleteMany({})
     // Use seeded user IDs to seed recipes with users
-    siteRecipeSeeds[0].user = userData.ops[0]._id
-    siteRecipeSeeds[1].user = userData.ops[1]._id
-    siteRecipeSeeds[2].user = userData.ops[1]._id
-    const data = await db.SiteRecipe.collection.insertMany(siteRecipeSeeds)
-    console.log('Seeded:', data.ops.length, 'recipes')
+    siteRecipeSeeds[0].user = userData.insertedIds["0"]
+    siteRecipeSeeds[1].user = userData.insertedIds["1"]
+    siteRecipeSeeds[2].user = userData.insertedIds["1"]
+    const recipeData = await db.SiteRecipe.collection.insertMany(siteRecipeSeeds)
+    console.log(recipeData)
+    console.log('Seeded:', recipeData.ops.length, 'recipes')
+    const something = await db.User.findOneAndUpdate(
+      { _id: userData.insertedIds["0"] },
+      { $push: {
+        
+
     process.exit(0);
   }
   catch (err) {
