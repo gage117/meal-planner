f2a7518276c60d6eca80ce95756f19f0f496eaf6
Gage Eide <gageeide@gmail.com>
2021-08-15 10:56:41 -0500

    Added setOpen state

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import NavBar from './components/NavBar';
 import Main from './pages/Main';
 import MenuContext from './utils/MenuContext';
 
 function App() {
+  const [open, setOpen] = useState(true);
   return (
     <>
       <MenuContext.Provider >
-        <NavBar />
-        <Main />
+        <NavBar setOpen={setOpen} open={open} />
+        <Main setOpen={setOpen} open={open} />
       </MenuContext.Provider>
     </>
   );
