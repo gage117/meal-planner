6e9894038fc4edb3b3a7cf63f43a5da9b4e8e9c4
Gage Eide <gageeide@gmail.com>
2021-08-16 09:40:52 -0500

    Added Card stuff to DayCard

--- client/src/components/DayCard/index.js
+++ client/src/components/DayCard/index.js
@@ -1,44 +1,51 @@
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import Grid from '@material-ui/core/Grid';
 import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 
 export default function DayCard({ day }) {
     return (
-        <Grid
-            container
-            direction="column"
-        >
-            { day }
-            <label>Breakfast</label>
-            <Select value={day.breakfast}>
-                <MenuItem value={'Bacon'}>Bacon</MenuItem>
-                <MenuItem value={'Eggs'}>Eggs</MenuItem>
-                <MenuItem value={'Omelette'}>Omelette</MenuItem>
-            </Select>
-            <label>Lunch</label>
-            <Select value={day.lunch}>
-                <MenuItem value={'Bacon'}>Bacon</MenuItem>
-                <MenuItem value={'Eggs'}>Eggs</MenuItem>
-                <MenuItem value={'Omelette'}>Omelette</MenuItem>
-            </Select>
-            <label>Dinner</label>
-            <Select value={day.dinner}>
-                <MenuItem value={'Bacon'}>Bacon</MenuItem>
-                <MenuItem value={'Eggs'}>Eggs</MenuItem>
-                <MenuItem value={'Omelette'}>Omelette</MenuItem>
-            </Select>
-            <label>Snackeroo 1</label>
-            <Select value={day.snackeroo}>
-                <MenuItem value={'Bacon'}>Bacon</MenuItem>
-                <MenuItem value={'Eggs'}>Eggs</MenuItem>
-                <MenuItem value={'Omelette'}>Omelette</MenuItem>
-            </Select>
-            <label>Snackeroo 2</label>
-            <Select value={day.snackeroo}>
-                <MenuItem value={'Bacon'}>Bacon</MenuItem>
-                <MenuItem value={'Eggs'}>Eggs</MenuItem>
-                <MenuItem value={'Omelette'}>Omelette</MenuItem>
-            </Select>
-        </Grid>
+        <Card>
+            <Grid
+                container
+                direction="column"
+            >
+                <CardHeader
+                    title={day}
+                />
+                <InputLabel id={`${day}Breakfast`}>Breakfast</InputLabel>
+                <Select labelId={`${day}Breakfast`}>
+                    <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                    <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                    <MenuItem value={'Omelette'}>Omelette</MenuItem>
+                </Select>
+                <InputLabel id={`${day}Lunch`}>Lunch</InputLabel>
+                <Select labelId={`${day}Lunch`}>
+                    <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                    <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                    <MenuItem value={'Omelette'}>Omelette</MenuItem>
+                </Select>
+                <InputLabel id={`${day}Dinner`}>Dinner</InputLabel>
+                <Select labelId={`${day}Dinner`}>
+                    <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                    <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                    <MenuItem value={'Omelette'}>Omelette</MenuItem>
+                </Select>
+                <InputLabel id={`${day}Snackeroo1`}>Snackeroo 1</InputLabel>
+                <Select labelId={`${day}Snackeroo1`}>
+                    <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                    <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                    <MenuItem value={'Omelette'}>Omelette</MenuItem>
+                </Select>
+                <InputLabel id={`${day}Snackeroo2`}>Snackeroo 2</InputLabel>
+                <Select labelId={`${day}Snackeroo2`}>
+                    <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                    <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                    <MenuItem value={'Omelette'}>Omelette</MenuItem>
+                </Select>
+            </Grid>
+        </Card>
     )
 }
