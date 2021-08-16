8d6004adcd16bec26975eedd0e05630cd444c5c5
Gage Eide <gageeide@gmail.com>
2021-08-15 22:16:58 -0500

    Started implementing DayCard component

--- client/src/components/DayCard/index.js
+++ client/src/components/DayCard/index.js
@@ -1,7 +1,44 @@
+import Grid from '@material-ui/core/Grid';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
 export default function DayCard({ day }) {
     return (
-        <div>
+        <Grid
+            container
+            direction="column"
+        >
             { day }
-        </div>
+            <label>Breakfast</label>
+            <Select value={day.breakfast}>
+                <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                <MenuItem value={'Omelette'}>Omelette</MenuItem>
+            </Select>
+            <label>Lunch</label>
+            <Select value={day.lunch}>
+                <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                <MenuItem value={'Omelette'}>Omelette</MenuItem>
+            </Select>
+            <label>Dinner</label>
+            <Select value={day.dinner}>
+                <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                <MenuItem value={'Omelette'}>Omelette</MenuItem>
+            </Select>
+            <label>Snackeroo 1</label>
+            <Select value={day.snackeroo}>
+                <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                <MenuItem value={'Omelette'}>Omelette</MenuItem>
+            </Select>
+            <label>Snackeroo 2</label>
+            <Select value={day.snackeroo}>
+                <MenuItem value={'Bacon'}>Bacon</MenuItem>
+                <MenuItem value={'Eggs'}>Eggs</MenuItem>
+                <MenuItem value={'Omelette'}>Omelette</MenuItem>
+            </Select>
+        </Grid>
     )
 }
