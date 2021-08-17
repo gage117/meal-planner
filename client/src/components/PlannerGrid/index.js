be28e90394fe48491b98fefa87a21e458a5d803f
Gage Eide <gageeide@gmail.com>
2021-08-16 22:59:57 -0500

    Set up plannerState

--- client/src/components/PlannerGrid/index.js
+++ client/src/components/PlannerGrid/index.js
@@ -1,18 +1,76 @@
+import { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import DayCard from '../DayCard';
 
+const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+
 export default function PlannerGrid() {
-  const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+  const [plannerState, setPlannerState] = useState({
+    sunday: {
+      breakfast: "",
+      lunch: "",
+      dinner: "",
+      snack1: "",
+      snack2: ""
+    },
+    monday: {
+      breakfast: "",
+      lunch: "",
+      dinner: "",
+      snack1: "",
+      snack2: ""
+    },
+    tuesday: {
+      breakfast: "",
+      lunch: "",
+      dinner: "",
+      snack1: "",
+      snack2: ""
+    },
+    wednesday: {
+      breakfast: "",
+      lunch: "",
+      dinner: "",
+      snack1: "",
+      snack2: ""
+    },
+    thursday: {
+      breakfast: "",
+      lunch: "",
+      dinner: "",
+      snack1: "",
+      snack2: ""
+    },
+    friday: {
+      breakfast: "",
+      lunch: "",
+      dinner: "",
+      snack1: "",
+      snack2: ""
+    },
+    saturday: {
+      breakfast: "",
+      lunch: "",
+      dinner: "",
+      snack1: "",
+      snack2: ""
+    }
+  });
 
   return (
     <Grid
       container
       justifyContent="space-evenly"
-      spacing={3}
+      spacing={4}
     >
       {days.map(day => (
         <Grid item xs={12} lg={3} key={day}>
-          <DayCard key={day} day={day} />
+          <DayCard 
+            key={day} 
+            day={day} 
+            plannerState={plannerState} 
+            setPlannerState={setPlannerState}  
+          />
         </Grid>
       ))}
     </Grid>
