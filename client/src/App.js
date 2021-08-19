8c45940c10b087f351015a23cbe472b65da45ba9
Gage Eide <gageeide@gmail.com>
2021-08-19 00:07:48 -0500

    Added ShoppingPage component and route

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import { useState, useReducer } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import {CssBaseline, Box} from '@material-ui/core';
 import NavBar from './components/NavBar';
 import {Drawer, drawerWidth} from './components/Drawer';
 import PlannerGrid from './pages/PlannerGrid';
+import ShoppingList from './pages/ShoppingList';
 import MealPlanContext from './utils/MealPlanContext';
 
 const reducer = (state, action) => {
@@ -74,7 +75,7 @@ const defaultState = {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   content: {
     flexGrow: 1,
@@ -103,25 +104,26 @@ function App() {
   const [mealPlan, dispatch] = useReducer(reducer, JSON.parse(localStorage.getItem("mealPlan")) || defaultState);
 
   return (
-    <div className={classes.root}>
-      <MealPlanContext.Provider value={{ mealPlan, dispatch }}>
-        <CssBaseline />
-        <NavBar setOpen={setOpen} open={open} />
-        <Drawer open={open} setOpen={setOpen} drawerWidth={drawerWidth} />
-        <main
-          className={clsx(classes.content, {
-            [classes.contentShift]: open,
-          })}
-        >
-          <Router>
-            <Switch>
-              <Route exact path="/" component={PlannerGrid} />
-              <Route path="/planner" component={PlannerGrid} />
-            </Switch>
-          </Router>
-        </main>
-      </MealPlanContext.Provider>
-    </div>
+    <MealPlanContext.Provider value={{ mealPlan, dispatch }}>
+      <Router>
+          <CssBaseline />
+          <NavBar setOpen={setOpen} open={open} />
+          <Box className={classes.root}>
+            <Drawer open={open} setOpen={setOpen} drawerWidth={drawerWidth} />
+            <main
+              className={clsx(classes.content, {
+                [classes.contentShift]: open,
+              })}
+              >
+              <Switch>
+                <Route exact path="/" component={PlannerGrid} />
+                <Route path="/planner" component={PlannerGrid} />
+                <Route path="/shopping-list" component={ShoppingList} />
+              </Switch>
+            </main>
+          </Box>
+      </Router>
+    </MealPlanContext.Provider>
   );
 }
 
---
+++ client/src/pages/ShoppingList/index.js
@@ -0,0 +1,56 @@
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Grid, 
+    Typography,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+    IconButton,
+} from '@material-ui/core';
+
+import { Delete as DeleteIcon, CheckCircle as CheckCircleIcon } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      maxWidth: 300,
+    },
+    demo: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+      margin: theme.spacing(4, 0, 2),
+    },
+  }));
+
+export default function ShoppingList() {
+    const classes = useStyles();
+
+    return (
+        <Grid container spacing={3}>
+            {['Breakfast', 'Lunch', 'Dinner', 'Snacks'].map((item) => {
+                return (
+                    <Grid item xs={12} md={6} className={classes.root}>
+                        <Typography variant="h6" className={classes.title}>
+                            {item}
+                        </Typography>
+                        <div className={classes.demo}>
+                            <List>
+                            {[0, 1, 2].map(index => (
+                                <ListItem key={index}>
+                                    <ListItemText primary="Single-line item" />
+                                    <ListItemSecondaryAction>
+                                        <IconButton edge="end" aria-label="delete">
+                                        <CheckCircleIcon />
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                                ))}
+                            </List>
+                        </div>
+                    </Grid>
+                )})}
+        </Grid>
+    )
+}
