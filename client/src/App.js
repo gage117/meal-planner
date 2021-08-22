6b27d77468cc4e43f14359b7a314a00e8dd8e6d6
Gage Eide <gageeide@gmail.com>
2021-08-22 16:18:03 -0500

    Cleaned up App.js of commented out code

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useReducer } from 'react';
+import { useState } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,71 +7,6 @@ import NavBar from './components/NavBar';
 import {Drawer, drawerWidth} from './components/Drawer';
 import PlannerGrid from './pages/PlannerGrid';
 import ShoppingList from './pages/ShoppingList';
-import {MealPlanContext} from './utils/MealPlanContext';
-
-// const reducer = (state, action) => {
-//   if (!action.day || !action.whichMeal || !action.mealName) {
-//     return state;
-//   }
-//   // Modify meal in state and then return copy of state
-//   state[action.day.toLowerCase()][action.whichMeal.toLowerCase()] = action.mealName;
-//   localStorage.setItem("mealPlan", JSON.stringify(state));
-//   return state = {
-//     ...state
-//   };
-// }
-
-// const defaultState = {
-//   sunday: {
-//     breakfast: "Fried Eggs",
-//     lunch: "Tuna Lunch Salad",
-//     dinner: "Keto Chili",
-//     snack1: "Beef Jerky",
-//     snack2: "Raspberries"
-//   },
-//   monday: {
-//     breakfast: "",
-//     lunch: "",
-//     dinner: "",
-//     snack1: "",
-//     snack2: ""
-//   },
-//   tuesday: {
-//     breakfast: "",
-//     lunch: "",
-//     dinner: "",
-//     snack1: "",
-//     snack2: ""
-//   },
-//   wednesday: {
-//     breakfast: "",
-//     lunch: "",
-//     dinner: "",
-//     snack1: "",
-//     snack2: ""
-//   },
-//   thursday: {
-//     breakfast: "",
-//     lunch: "",
-//     dinner: "",
-//     snack1: "",
-//     snack2: ""
-//   },
-//   friday: {
-//     breakfast: "",
-//     lunch: "",
-//     dinner: "",
-//     snack1: "",
-//     snack2: ""
-//   },
-//   saturday: {
-//     breakfast: "",
-//     lunch: "",
-//     dinner: "",
-//     snack1: "",
-//     snack2: ""
-//   }
-// }
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -97,15 +32,11 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
-  console.log("Rendering!");
 
   // Drawer open/close state
   const [open, setOpen] = useState(true);
-  // Initialize meal plan from local storage or set to default template
-  // const [mealPlan, dispatch] = useReducer(reducer, JSON.parse(localStorage.getItem("mealPlan")) || defaultState);
 
   return (
-    // <MealPlanContext.Provider value={{ mealPlan, dispatch }}>
       <Router>
           <CssBaseline />
           <NavBar setOpen={setOpen} open={open} />
@@ -124,7 +55,6 @@ function App() {
             </main>
           </Box>
       </Router>
-    // </MealPlanContext.Provider>
   );
 }
 
