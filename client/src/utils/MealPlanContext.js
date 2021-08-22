87c49b6dfcc39f67f9429003e1aa03424704cead
Gage Eide <gageeide@gmail.com>
2021-08-22 17:25:45 -0500

    Imported template and changed format of state

--- client/src/utils/MealPlanContext.js
+++ client/src/utils/MealPlanContext.js
@@ -1,56 +1,244 @@
 import { createContext, useReducer } from "react";
+import {template as meals} from './Foodroid_Template';
 
 export const MealPlanContext = createContext();
 
 const defaultState = {
   sunday: {
-    breakfast: "Fried Eggs",
-    lunch: "Tuna Lunch Salad",
-    dinner: "Keto Chili",
-    snack1: "Beef Jerky",
-    snack2: "Raspberries"
+    breakfast: {
+      name: "Fried Eggs",
+      instructions: [
+        "Write out step 1",
+        "Write out step 2",
+        "Write out step 3"
+      ],
+      mealOfTheDay: "Breakfast",
+      ingredients: [
+        {
+          name: "Egg",
+          quantity: 2,
+          unit: "",
+        },
+        {
+          name: "Butter",
+          quantity: 1,
+          unit: "tbsp",
+        },
+      ],
+      tags: [
+        "egg",
+        "butter",
+        "healthy",
+        "low-calorie",
+        "low-carb",
+        "low-fat",
+      ],
+      user: { _id: "" },
+    },
+    lunch: {
+      name: "Tuna Lunch Salad",
+      instructions: [
+        "Write out step 1",
+        "Write out step 2",
+        "Write out step 3"
+      ],
+      mealOfTheDay: "Lunch",
+      ingredients: [
+        {
+          name: "Tuna (in brine)",
+          quantity: 1,
+          unit: "can",
+        },
+        {
+          name: "Olive Oil",
+          quantity: 1,
+          unit: "tsp",
+        },
+        {
+          name: "Spinach",
+          quantity: 1,
+          unit: "cup",
+        },
+        {
+          name: "Feta Cheese chunks",
+          quantity: .25,
+          unit: "cup",
+        },
+        {
+          name: "Dijon Mustard",
+          quantity: 1,
+          unit: "tsp",
+        },
+        {
+          name: "Avocado",
+          quantity: 1,
+          unit: "tsp",
+        }
+      ],
+      tags: [
+        "tuna",
+        "salad",
+        "healthy",
+        "low-calorie",
+        "low-carb",
+        "low-fat",
+      ],
+      user: { _id: "" },
+    },
+    dinner: {
+      name: "Keto Chili",
+      instructions: [
+        "Write out step 1",
+        "Write out step 2",
+        "Write out step 3"
+      ],
+      mealOfTheDay: "Dinner",
+      ingredients: [
+        {
+          name: "Garlic",
+          quantity: 2,
+          unit: "cloves",
+        },
+        {
+          name: "Onion",
+          quantity: .5,
+          unit: "",
+        },
+        {
+          name: "Ground Beef",
+          quantity: 1,
+          unit: "lb",
+        },
+        {
+          name: "Diced Tomato",
+          quantity: 8,
+          unit: "oz",
+        },
+        {
+          name: "Cumin",
+          quantity: 1,
+          unit: "tsp",
+        },
+        {
+          name: "Paprika",
+          quantity: .25,
+          unit: "tsp",
+        },
+        {
+          name: "Oregano",
+          quantity: .25,
+          unit: "tsp",
+        },
+        {
+          name: "Chipotle Powder",
+          quantity: .25,
+          unit: "tsp",
+        },
+        {
+          name: "Beef Broth",
+          quantity: 16,
+          unit: "fl oz",
+        }
+      ],
+      tags: [
+        "chili",
+        "beef",
+        "healthy",
+        "low-calorie",
+        "low-carb",
+        "low-fat",
+      ],
+      user: { _id: "" },
+    },
+    snack1: {
+      name: "Beef Jerky",
+      instructions: [
+        "Write out step 1",
+        "Write out step 2",
+        "Write out step 3"
+      ],
+      mealOfTheDay: "Snack",
+      ingredients: [
+        {
+          name: "Beef jerky",
+          quantity: 2,
+          unit: "oz",
+        }
+      ],
+      tags: [
+        "meat",
+        "vegan",
+        "low-carb",
+      ],
+      user: { _id: "" },
+    },
+    snack2: {
+      name: "String Cheese",
+      instructions: [
+        "Write out step 1",
+        "Write out step 2",
+        "Write out step 3"
+      ],
+      mealOfTheDay: "Snack",
+      ingredients: [
+        {
+          name: "String Cheese",
+          quantity: 1,
+          unit: "",
+        }
+      ],
+      tags: [
+        "dairy",
+        "healthy",
+        "low-calorie",
+        "low-carb",
+        "low-fat",
+        "snack"
+      ],
+      user: { _id: "" },
+    }
   },
   monday: {
-    breakfast: "",
-    lunch: "",
-    dinner: "",
-    snack1: "",
-    snack2: ""
+    breakfast: {},
+    lunch: {},
+    dinner: {},
+    snack1: {},
+    snack2: {}
   },
   tuesday: {
-    breakfast: "",
-    lunch: "",
-    dinner: "",
-    snack1: "",
-    snack2: ""
+    breakfast: {},
+    lunch: {},
+    dinner: {},
+    snack1: {},
+    snack2: {}
   },
   wednesday: {
-    breakfast: "",
-    lunch: "",
-    dinner: "",
-    snack1: "",
-    snack2: ""
+    breakfast: {},
+    lunch: {},
+    dinner: {},
+    snack1: {},
+    snack2: {}
   },
   thursday: {
-    breakfast: "",
-    lunch: "",
-    dinner: "",
-    snack1: "",
-    snack2: ""
+    breakfast: {},
+    lunch: {},
+    dinner: {},
+    snack1: {},
+    snack2: {}
   },
   friday: {
-    breakfast: "",
-    lunch: "",
-    dinner: "",
-    snack1: "",
-    snack2: ""
+    breakfast: {},
+    lunch: {},
+    dinner: {},
+    snack1: {},
+    snack2: {}
   },
   saturday: {
-    breakfast: "",
-    lunch: "",
-    dinner: "",
-    snack1: "",
-    snack2: ""
+    breakfast: {},
+    lunch: {},
+    dinner: {},
+    snack1: {},
+    snack2: {}
   }
 }
 
