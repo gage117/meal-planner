6df74891f540c282bb7e70b90bff40f16264b009
Gage Eide <gageeide@gmail.com>
2021-08-22 15:13:04 -0500

    Implemented mock data

--- client/src/pages/ShoppingList/index.js
+++ client/src/pages/ShoppingList/index.js
@@ -8,14 +8,23 @@ import {
     ListItemText,
     ListItemSecondaryAction,
     IconButton,
+    Box,
 } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
+import clsx from 'clsx';
 import { CheckCircle as CheckCircleIcon } from '@material-ui/icons';
+
+// !!!
+import {template} from './Foodroid_Template';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
-      maxWidth: 300,
+      '& .onHand.true' : {
+          color: 'green'
+      },
+      '& .onHand.false' : {
+          color: 'red'
+      },
     },
     demo: {
       backgroundColor: theme.palette.background.paper,
@@ -24,171 +33,6 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(4, 0, 2),
     },
   }));
-
-const mockPopulatedMealPlan = {
-    breakfasts: [
-        {
-        name: "Keto Egg Muffins",
-        instructions: [
-            "Write out step 1",
-            "Write out step 2",
-            "Write out step 3"
-        ],
-        mealOfTheDay: "Breakfast",
-        ingredients: [
-            {
-            name: "Sweet Potato",
-            quantity: 1,
-            unit: "",
-            },
-            {
-            name: "Grated Cheddar",
-            quantity: .25,
-            unit: "cup",
-            },
-            {
-            name: "Bacon",
-            quantity: 2,
-            unit: "strips",
-            },
-            {
-            name: "Eggs",
-            quantity: 1,
-            unit: "",
-            },
-            {
-            name: "Heavy Whipping Cream",
-            quantity: 1,
-            unit: "tbsp",
-            }
-        ],
-        tags: [
-            "egg",
-            "muffins",
-            "healthy",
-            "low-calorie",
-            "low-carb",
-            "low-fat",
-        ],
-        user: { _id: "" },
-        }
-    ],
-    lunches: [
-        {
-        name: "Tuna Lunch Salad",
-        instructions: [
-            "Write out step 1",
-            "Write out step 2",
-            "Write out step 3"
-        ],
-        mealOfTheDay: "Lunch",
-        ingredients: [
-            {
-            name: "Tuna (in brine)",
-            quantity: 1,
-            unit: "can",
-            },
-            {
-            name: "Olive Oil",
-            quantity: 1,
-            unit: "tsp",
-            },
-            {
-            name: "Spinach",
-            quantity: 1,
-            unit: "cup",
-            },
-            {
-            name: "Feta Cheese chunks",
-            quantity: .25,
-            unit: "cup",
-            },
-            {
-            name: "Dijon Mustard",
-            quantity: 1,
-            unit: "tsp",
-            },
-            {
-            name: "Avocado",
-            quantity: 1,
-            unit: "tsp",
-            }
-        ],
-        tags: [
-            "tuna",
-            "salad",
-            "healthy",
-            "low-calorie",
-            "low-carb",
-            "low-fat",
-        ],
-        user: { _id: "" },
-        }
-    ],
-    dinners: [
-        {
-        name: "Baked Salmon",
-        instructions: [
-            "Write out step 1",
-            "Write out step 2",
-            "Write out step 3"
-        ],
-        mealOfTheDay: "Dinner",
-        ingredients: [
-            {
-            name: "Salmon",
-            quantity: 4,
-            unit: "oz",
-            },
-            {
-            name: "Asparagus",
-            quantity: 3,
-            unit: "oz",
-            },
-            {
-            name: "Parmesan Cheese",
-            quantity: .25,
-            unit: "cup",
-            }
-        ],
-        tags: [
-            "salmon",
-            "asparagus",
-            "healthy",
-            "low-calorie",
-            "low-carb",
-            "low-fat",
-        ],
-        user: { _id: "" },
-        }
-    ],
-    snacks: [
-        {
-        name: "Hard Boiled Eggs",
-        instructions: [
-            "Write out step 1",
-            "Write out step 2",
-            "Write out step 3"
-        ],
-        mealOfTheDay: "Snack",
-        ingredients: [
-            {
-            name: "Eggs",
-            quantity: 4,
-            unit: "",
-            }
-        ],
-        tags: [
-            "eggs",
-            "healthy",
-            "low-calorie",
-            "low-carb",
-            "low-fat",
-        ],
-        user: { _id: "" },
-        }
-    ]
-}
 
 const rows = [
     { id:1, ingredient: 'Carrots', quantity: 3, unit: "" },
@@ -200,47 +44,56 @@ const rows = [
     { field: 'id', headerName: 'ID', hide: true },
     { field: 'ingredient', headerName: 'Ingredient', width: 250 },
     { field: 'quantity', headerName: 'Quantity', width: 140 },
-    { field: 'unit', headerName: 'Unit', width: 150 },
-    { field: 'haveOnHand', headerName: 'Have On Hand', type: 'boolean', isEditable: true, width: 150 },
+    { field: 'unit', headerName: 'Unit', width: 130 },
+    { field: 'haveOnHand', headerName: 'Have On Hand', type: 'boolean', editable: true, width: 150,
+        cellClassName: (params) => {
+            clsx('onHand', {
+                false: !params.value,
+                true: !!params.value,
+            })
+        },    
+    },
   ];
 
 function reduceIngredients(mealPlan) {
     const ingredientTotals = {};
-    // Go over each meal of the day
-    Object.values(mealPlan).map(mealArray => {
-        console.log(mealArray);
+    let idIncrementer = 0;
+    // Go over each meal of the day\
+    Object.values(mealPlan.meals).map(mealArray => {
         // Go over each meal in that array
+        console.log("MealArray: ", mealArray);
         mealArray.map(meal => {
-            console.log(meal);
+            console.log("Meal: ", meal);
             // Go over each ingredient in that meal
-            meal.ingredients.forEach(ingredient => {
+            meal.ingredients.map((ingredient) => {
                 // TODO: Implement unit conversion
                 if (ingredientTotals[ingredient.name] && ingredientTotals[ingredient.name].unit === ingredient.unit ) {
                     ingredientTotals[ingredient.name].quantity += ingredient.quantity;
                 } else {
                     ingredientTotals[ingredient.name] = {
-                        name: ingredient.name,
+                        id: idIncrementer,
+                        ingredient: ingredient.name,
                         quantity: ingredient.quantity,
                         unit: ingredient.unit,
                     };
+                    idIncrementer++;
                 }
             })
         })
     })
-    return Object.values(ingredientTotals).sort((a, b) => a.name.localeCompare(b.name));
+    return Object.values(ingredientTotals).sort((a, b) => a.ingredient.localeCompare(b.ingredient));
 }
 
 export default function ShoppingList() {
     const classes = useStyles();
 
-    mockPopulatedMealPlan.ingredientTotals = reduceIngredients(mockPopulatedMealPlan);
-    console.log(mockPopulatedMealPlan.ingredientTotals);
+    template.ingredientTotals = reduceIngredients(template);
 
     return (
         <MealPlannerContext.Consumer>
             {({mealPlan}) => (
-                <>
-                <DataGrid rows={rows} columns={columns} autoHeight checkboxSelection/>
+                <Box className={classes.root}>
+                {/* <DataGrid rows={template.ingredientTotals} columns={columns} autoHeight checkboxSelection/> */}
                 <Grid container spacing={3}>
                     <Grid item xs={12} md={6} className={classes.root}>
                         <Typography variant="h6" className={classes.title}>
@@ -248,9 +101,9 @@ export default function ShoppingList() {
                         </Typography>
                         <div className={classes.demo}>
                             <List>
-                            {mockPopulatedMealPlan.ingredientTotals.map((ingredient, index) => (
+                            {template.ingredientTotals.map((ingredient, index) => (
                                 <ListItem key={index}>
-                                    <ListItemText primary={ingredient.name} secondary={`Quantity: ${ingredient.quantity} ${ingredient.unit}`}/>
+                                    <ListItemText primary={ingredient.ingredient} secondary={`Quantity: ${ingredient.quantity} ${ingredient.unit}`}/>
                                     <ListItemSecondaryAction>
                                         <IconButton edge="end" aria-label="delete">
                                         <CheckCircleIcon />
@@ -262,7 +115,7 @@ export default function ShoppingList() {
                         </div>
                     </Grid>
                 </Grid>
-                </>
+                </Box>
             )}
         </MealPlannerContext.Consumer>
     )
