72ea44429de32b181b90fbf8b494a704efa59a70
gage117 <gageeide@gmail.com>
2021-10-06 17:36:37 -0500

    Added Auth0Provider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 import MealPlanProvider from './utils/MealPlanContext';
 
 ReactDOM.render(
   // <React.StrictMode>
-  <MealPlanProvider>
-    <App />
-  </MealPlanProvider>
+  <Auth0Provider
+    domain="foodroid.us.auth0.com"
+    clientId="0ie5UArn2bUnK0StLr6PKnFbfdOLCBNE"
+    redirectUri={window.location.origin}
+    audience="https://foodroid.us.auth0.com/api/v2/"
+    scope="read:current_user update:current_user_metadata"
+  >
+    <MealPlanProvider>
+      <App />
+    </MealPlanProvider>
+  </Auth0Provider>,
   // </React.StrictMode>,
-  ,
   document.getElementById('root')
 );
