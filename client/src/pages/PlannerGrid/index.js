05dd0864e3ac5deb224acf31c89659e872906b01
gage117 <57643501+gage117@users.noreply.github.com>
2021-10-11 22:35:14 -0500

    Updated planner grid component before switching branches

--- client/src/pages/PlannerGrid/index.js
+++ client/src/pages/PlannerGrid/index.js
@@ -1,27 +1,57 @@
-import Grid from '@material-ui/core/Grid';
+import { Button, Grid } from '@material-ui/core';
 import DayCard from '../../components/DayCard';
-import {template} from '../../utils/Foodroid_Template';
+import template from '../../utils/seeds-gage';
 
 const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 
 export default function PlannerGrid() {
   // TODO: Set meals from database call
+  const meals = {
+    breakfasts: [],
+    lunches: [],
+    dinners: [],
+    snacks: [],
+  };
+  let i = 0, len = template.length;
+  while (i < len) {
+    console.log(template[i])
+    switch (template[i].mealOfTheDay) {
+      case 'Breakfast':
+        meals.breakfasts.push(template[i]);
+        break;
+      case 'Lunch':
+        meals.lunches.push(template[i]);
+        break;
+      case 'Dinner':
+        meals.dinners.push(template[i]);
+        break;
+      case 'Snack':
+        meals.snacks.push(template[i]);
+        break;
+      default:
+        break;
+    }
+    i++;
+  }
 
   return (
-    <Grid
-    container
-    justifyContent="space-evenly"
-    spacing={4}
-  >
-    {days.map(day => (
-      <Grid item xs={12} lg={3} key={day}>
-        <DayCard 
-          key={day} 
-          day={day} 
-          meals={template.meals}
-        />
+    <>
+      <Button variant="contained" color="primary">Clear Meal Plan</Button>
+      <Grid
+        container
+        justifyContent="space-evenly"
+        spacing={4}
+      >
+        {days.map(day => (
+          <Grid item xs={12} lg={3} key={day}>
+            <DayCard 
+              key={day} 
+              day={day} 
+              meals={meals}
+            />
+          </Grid>
+        ))}
       </Grid>
-    ))}
-  </Grid>
+    </>
   )
 }
