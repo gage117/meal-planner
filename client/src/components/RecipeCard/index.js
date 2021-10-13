8ff08e39d915dc4bff81188400f5def42474e870
gage117 <57643501+gage117@users.noreply.github.com>
2021-10-13 18:37:49 -0500

    Added some data do the RecipeCard component

--- client/src/components/RecipeCard/index.js
+++ client/src/components/RecipeCard/index.js
@@ -16,6 +16,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { ListItem } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,9 +26,15 @@ const useStyles = makeStyles((theme) => ({
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
+  autoMarginLeft: {
+    marginLeft: 'auto'
+  },
+  removeTopAndBottomMargin: {
+    marginTop: 0,
+    marginBottom: 0
+  },
   expand: {
     transform: 'rotate(0deg)',
-    marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.shortest,
     }),
@@ -51,13 +58,7 @@ export default function RecipeCard({ meal }) {
   return (
     <Card className={classes.root}>
       <CardHeader
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
         title={meal.name}
-        subheader="September 14, 2016"
       />
       {/* <CardMedia
         className={classes.media}
@@ -66,6 +67,7 @@ export default function RecipeCard({ meal }) {
       /> */}
       <CardContent>
         {meal.tags.map(tag => (<Chip label={tag} variant="outlined" />))}
+        <Typography paragraph color="textSecondary">{meal.description}</Typography>
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
@@ -74,6 +76,7 @@ export default function RecipeCard({ meal }) {
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
+        <Typography className={classes.autoMarginLeft} >More Info</Typography>
         <IconButton
           className={clsx(classes.expand, {
             [classes.expandOpen]: expanded,
@@ -87,29 +90,28 @@ export default function RecipeCard({ meal }) {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+          <Typography variant="h4">Instructions</Typography>
+          <hr />
+          <ul>
+            {meal.instructions.map(instruction => {
+              return (
+                <li>
+                  <Typography paragraph>{instruction}</Typography>
+                </li>
+              )
+            })}
+          </ul>
+          <Typography variant="h4">Ingredients</Typography>
+          <hr />
+          <ul>
+            {meal.ingredients.map(({name, unit, quantity}) => {
+              return (
+                <li>
+                  <Typography paragraph>{`${name}: ${quantity} ${unit}`}</Typography>
+                </li>
+              )
+            })}
+          </ul>
         </CardContent>
       </Collapse>
     </Card>
