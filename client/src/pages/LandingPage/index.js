87d5569460dfdb8b03d973c6f12cf4a2bac5b8b7
gage117 <57643501+gage117@users.noreply.github.com>
2021-10-26 23:14:08 -0500

    Added note to landing page

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -2,7 +2,7 @@ const LandingPage = () => {
   return (
     <div>
       <h1>Hello!</h1>
-      <h3>I am a landing page</h3>
+      <h3>This site is still in progress, but at the moment feel free to explore the Meal Planner and the Shopping List that it generates. There's also a Saved Recipes page where you can take a peek at all of the default recipes at the moment, and even login/signup through Auth0.</h3>
     </div>
   )
 }
