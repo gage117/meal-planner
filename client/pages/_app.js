26394b7d9973b045a882293398cf28541d20ff31
gage117 <57643501+gage117@users.noreply.github.com>
2021-11-16 14:12:03 -0600

    imported drawer component with no compilation errors

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,15 +1,50 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
 import Head from 'next/head';
-import CssBaseline from '@mui/material/CssBaseline';
+import { makeStyles } from '@mui/styles';
+import { CssBaseline, Box } from '@mui/material';
 import { CacheProvider } from '@emotion/react';
 import createEmotionCache from '../utils/createEmotionCache';
 
 import NavBar from '../components/navbar';
+import {Drawer, drawerWidth} from '../components/drawer';
 
 const clientSideEmotionCache = createEmotionCache();
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex"
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
 export default function MyApp(props) {
+  const classes = useStyles();
+  // const { isLoading } = useAuth0();
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  // Drawer open/close state
+  const [open, setOpen] = useState(false);
+
+  // if (isLoading) {
+  //   return <div>Loading ...</div>;
+  // }
 
   return <CacheProvider value={emotionCache}> 
     <Head>
@@ -18,8 +53,17 @@ export default function MyApp(props) {
     </Head>
     <>
       <CssBaseline />
-      <NavBar />
-      <Component {...pageProps} />
+      <NavBar setOpen={setOpen} open={open} />
+      <Box className={classes.root}>
+        <Drawer open={open} setOpen={setOpen} drawerWidth={drawerWidth} />
+        <Component
+          {...pageProps}
+          className={clsx(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        />
+      </Box>
+      {/* <Component {...pageProps} /> */}
     </>
   </CacheProvider>
 }
