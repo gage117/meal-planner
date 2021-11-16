8b77c41725e42ccd9a44b5619e5f66d30406b2f3
gage117 <gageeide@gmail.com>
2021-11-16 01:17:00 -0600

    _app skeleton

---
+++ client/src/pages/_app.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+export default function MyApp(props) {
+   <> 
+    <Head>
+      <title>My page</title>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+    </Head>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+}
