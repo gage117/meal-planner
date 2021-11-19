6a9ef46dff9c8faeb8b34439c2cca4d1d4eb336c
gage117 <57643501+gage117@users.noreply.github.com>
2021-11-18 18:27:48 -0600

    Removed unneeded ThemeContext file

--- client/utils/ThemeContext.js
+++
@@ -1,9 +0,0 @@
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme({ spacing: 8 });
-
-export default function Container({ Component, pageProps }) {
-  return <ThemeProvider theme={theme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
-}
