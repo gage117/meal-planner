b001ff9fdfd59b90abdf9ac0db992770fa86925d
gage117 <57643501+gage117@users.noreply.github.com>
2021-12-09 22:26:16 -0600

    Added more necessary .env variables

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 
 module.exports = (on, config) => {
-  config.env.auth0_username = process.env.AUTH0_USERNAME
-  config.env.auth0_password = process.env.AUTH0_PASSWORD
+  config.env.auth0_username = process.env.AUTH0_TEST_USERNAME
+  config.env.auth0_password = process.env.AUTH0_TEST_PASSWORD
   config.env.auth0_domain = process.env.AUTH0_ISSUER_BASE_URL
-  config.env.auth0_client_id = process.env.AUTH0_CLIENT_ID
-  config.env.auth0_client_secret = process.env.AUTH0_CLIENT_SECRET
+  config.env.auth0_audience = process.env.AUTH0_TEST_AUDIENCE
+  config.env.auth0_scope = process.env.AUTH0_TEST_SCOPE
+  config.env.auth0_client_id = process.env.AUTH0_TEST_CLIENT_ID
+  config.env.auth0_client_secret = process.env.AUTH0_TEST_CLIENT_SECRET
 
   return config
 }
