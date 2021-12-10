9994d4e0b54610be79cd45ce0d749895dc97bb42
gage117 <57643501+gage117@users.noreply.github.com>
2021-12-09 18:14:13 -0600

    Added environment variables to Cypress config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,11 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+require('dotenv').config()
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  config.env.auth0_username = process.env.AUTH0_USERNAME
+  config.env.auth0_password = process.env.AUTH0_PASSWORD
+  config.env.auth0_domain = process.env.AUTH0_ISSUER_BASE_URL
+  config.env.auth0_client_id = process.env.AUTH0_CLIENT_ID
+  config.env.auth0_client_secret = process.env.AUTH0_CLIENT_SECRET
+
+  return config
 }
