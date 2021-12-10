f54bddd58dffd6faf2876eb578c8f263319b81d3
gage117 <57643501+gage117@users.noreply.github.com>
2021-12-09 18:14:45 -0600

    Added loginByAuth0Api command to Cypress, have to see how to get password grant type working through auth0

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,8 +1,14 @@
 describe('My First Test', () => {
+  beforeEach(function () {
+    cy.loginByAuth0Api(
+      Cypress.env('auth0_username'),
+      Cypress.env('auth0_password')
+    )
+  })
+
   it('Visits the home page', () => {
     cy.visit('http://localhost:3000');
 
     cy.get("#menuButton").click();
-    cy.contains("Login").click();
   })
 })
